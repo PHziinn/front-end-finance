1b47b3f1bfcf85ce3ef2cb390fc483b58ac018e2
skamikaze4@gmail.com <skamikaze4@gmail.com>
2024-01-15 08:10:10 -0300

    Alteração

--- src/components/Information/index.jsx
+++ src/components/Information/index.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useSomaFinance } from "../../hooks/useSomaFinance"
 import { useConvertValues } from "../../hooks/useConvertValues"
+import EntradaSvg from "../../assets/img/entrada.svg"
+import SaidaSvg from "../../assets/img/saida.svg"
+import TotalSvg from "../../assets/img/total.svg"
 
 export const FinanceInfornation = ({listSoma}) => {
   const { ganho, despesas, result} = useSomaFinance(listSoma)
@@ -14,7 +17,7 @@ export const FinanceInfornation = ({listSoma}) => {
           <h3 className="font-normal text-base flex items-center justify-between">
 
             <span>Entradas</span>
-            <img src="/src/assets/img/entrada.svg" alt="Imagem de Entrada" />
+            <img src={EntradaSvg} alt="Imagem de Entrada" />
           </h3>
           <p className="text-3xl leading-10 mt-4" >{convertValues(Number(ganho))}</p>
 
@@ -24,7 +27,7 @@ export const FinanceInfornation = ({listSoma}) => {
           <h3 className="font-normal text-base flex items-center justify-between">
 
             <span>Saída</span>
-            <img src="/src/assets/img/saida.svg" alt="Imagem de Saída" />
+            <img src={SaidaSvg} alt="Imagem de Saída" />
           </h3>
           <p className="text-3xl  leading-10 mt-4" >{convertValues(Number(despesas))}</p>
           
@@ -35,7 +38,7 @@ export const FinanceInfornation = ({listSoma}) => {
 
           <h3 className="font-normal text-base flex items-center justify-between">
             <span>Total</span>
-            <img src="/src/assets/img/total.svg" alt="Imagem Total" />
+            <img src={TotalSvg} alt="Imagem Total" />
           </h3>
           <p className="text-3xl leading-10 mt-4" >{convertValues(Number(result))}</p>
 
